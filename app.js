819292daeec1fbdfddd0e9dca46dfcd2cfca6024
fazlay26 <rabboi007@gmail.com>
2022-03-01 01:02:59 +0600

    main-part

---
+++ app.js
@@ -0,0 +1,75 @@
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchFieldText = searchField.value;
+    //console.log(searchFieldText)
+    // remove searchText
+    searchField.value = '';
+
+    // loadData
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneResult(data.data))
+
+}
+// displayData
+const displayPhoneResult = phones => {
+    //console.log(phones)
+    const phoneContainer = document.getElementById('phone-container');
+
+    for (const phone of phones) {
+        //console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ` <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+                <h5>Brand:${phone.brand}</h5>
+                <button onclick="phoneDetails('${phone.slug}')" class="btn-warning">details</button>
+        </div>
+
+    </div>`;
+        phoneContainer.appendChild(div)
+
+    }
+}
+// load single phone details
+const phoneDetails = detail => {
+    //console.log(detail)
+    const url = `https://openapi.programming-hero.com/api/phone/${detail}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data))
+}
+// display single phoneDetails
+const displayPhoneDetail = info => {
+    console.log(info)
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `<img src="${info.data.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${info.data.releaseDate}</h5>
+      <h5 class="card-title">${info.data.name}</h5>
+
+      <h5>sensors:</h5>
+      <ul><li>${info.data.mainFeatures.sensors[0]}</li></ul>
+      <ul><li>${info.data.mainFeatures.sensors[1]}</li></ul>
+      <ul><li>${info.data.mainFeatures.sensors[2]}</li></ul>
+      <ul><li>${info.data.mainFeatures.sensors[3]}</li></ul>
+      <ul><li>${info.data.mainFeatures.sensors[4]}</li></ul>
+      <ul><li>${info.data.mainFeatures.sensors[5]}</li></ul>
+      <h5>Display Size</h5>
+      <p>${info.data.mainFeatures.displaySize}</p>
+      <h5>Memory</h5>
+      <p>${info.data.mainFeatures.memory}</p>
+      <h5>Storage</h5>
+      <p>${info.data.mainFeatures.storage}</p>
+
+
+
+    </div>`;
+    phoneDetails.appendChild(div);
+
+}
